0d8faee1b6a47171e1733e30c102268ac9288140
yuan cheng <chengyuan82281681@hotmail.com>
2018-12-17 16:49:55 +1100

    FIRST RELEASE of the label printer program, now can only work with default label printer.

--- LabelReprint/Form1.Designer.cs
+++ LabelReprint/Form1.Designer.cs
@@ -1,6 +1,6 @@
 ﻿namespace LabelReprint
 {
-    partial class Form1
+    partial class LabelPrinter
     {
         /// <summary>
         /// Required designer variable.
@@ -28,38 +28,90 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(LabelPrinter));
+            this.printPreviewDialog1 = new System.Windows.Forms.PrintPreviewDialog();
+            this.printDocument1 = new System.Drawing.Printing.PrintDocument();
+            this.button1 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.ErrMsg = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
-            // tableLayoutPanel1
+            // printPreviewDialog1
             // 
-            this.tableLayoutPanel1.ColumnCount = 2;
-            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 15F));
-            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 85F));
-            this.tableLayoutPanel1.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.tableLayoutPanel1.Location = new System.Drawing.Point(0, 0);
-            this.tableLayoutPanel1.Name = "tableLayoutPanel1";
-            this.tableLayoutPanel1.RowCount = 2;
-            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 90F));
-            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 10F));
-            this.tableLayoutPanel1.Size = new System.Drawing.Size(538, 257);
-            this.tableLayoutPanel1.TabIndex = 0;
+            this.printPreviewDialog1.AutoScrollMargin = new System.Drawing.Size(0, 0);
+            this.printPreviewDialog1.AutoScrollMinSize = new System.Drawing.Size(0, 0);
+            this.printPreviewDialog1.ClientSize = new System.Drawing.Size(400, 300);
+            this.printPreviewDialog1.Document = this.printDocument1;
+            this.printPreviewDialog1.Enabled = true;
+            this.printPreviewDialog1.Icon = ((System.Drawing.Icon)(resources.GetObject("printPreviewDialog1.Icon")));
+            this.printPreviewDialog1.Name = "printPreviewDialog1";
+            this.printPreviewDialog1.Visible = false;
+            this.printPreviewDialog1.Load += new System.EventHandler(this.printPreviewDialog1_Load);
             // 
-            // Form1
+            // printDocument1
+            // 
+            this.printDocument1.PrintPage += new System.Drawing.Printing.PrintPageEventHandler(this.printDocument1_PrintPage);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(259, 95);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(100, 46);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "PRINT";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(28, 112);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(225, 20);
+            this.textBox1.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AccessibleRole = System.Windows.Forms.AccessibleRole.None;
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(25, 95);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(84, 13);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Coil ID Scanned";
+            // 
+            // ErrMsg
+            // 
+            this.ErrMsg.AutoSize = true;
+            this.ErrMsg.Location = new System.Drawing.Point(28, 139);
+            this.ErrMsg.Name = "ErrMsg";
+            this.ErrMsg.Size = new System.Drawing.Size(0, 13);
+            this.ErrMsg.TabIndex = 3;
+            // 
+            // LabelPrinter
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(538, 257);
-            this.Controls.Add(this.tableLayoutPanel1);
-            this.Name = "Form1";
+            this.Controls.Add(this.ErrMsg);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button1);
+            this.Name = "LabelPrinter";
             this.Text = "Label Printer";
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
 
-        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel1;
+        private System.Windows.Forms.PrintPreviewDialog printPreviewDialog1;
+        private System.Drawing.Printing.PrintDocument printDocument1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label ErrMsg;
     }
 }
 
--- LabelReprint/Form1.cs
+++ LabelReprint/Form1.cs
@@ -3,18 +3,95 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.Drawing.Printing;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using Zen.Barcode;
 
 namespace LabelReprint
 {
-    public partial class Form1 : Form
+    public partial class LabelPrinter : Form
     {
-        public Form1()
+        public LabelPrinter()
         {
             InitializeComponent();
+        }
+
+        private void printDocument1_PrintPage(object sender, System.Drawing.Printing.PrintPageEventArgs e)
+        {
+            Graphics g = e.Graphics;
+
+            CodeQrBarcodeDraw QRcode = BarcodeDrawFactory.CodeQr; // to generate QR code
+            Image QRcodeImage = QRcode.Draw(textBox1.Text, 100);
+            // RectangleF(The coordinates of the upper-left corner of the rectangle, width, height)
+            RectangleF QRcodeRect = new RectangleF(50.0F, 40.0F, 150.0F, 150.0F);
+            g.DrawImage(QRcodeImage, QRcodeRect);
+
+            BarcodeDraw bdraw = BarcodeDrawFactory.GetSymbology(BarcodeSymbology.Code128);
+            Image barcodeImage = bdraw.Draw(textBox1.Text, 100);
+            RectangleF barcodeRect = new RectangleF(250.0F, 40.0F, 400.0F, 100.0F);
+            g.DrawImage(barcodeImage, barcodeRect);
+
+            // Create string to draw.
+            String drawString = textBox1.Text;
+
+            // Create font and brush.
+            Font drawFont = new Font("Times New Roman", 16);
+            SolidBrush drawBrush = new SolidBrush(Color.Black);
+
+            // Create point for upper-left corner of drawing.
+            float x = 250.0F;
+            float y = 150.0F;
+            g.DrawString(drawString, drawFont, drawBrush, x, y);
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (textBox1.Text != "")
+            {
+                ErrMsg.Text = "";
+                Printing();
+                //printPreviewDialog1.ShowDialog();
+            }
+            else
+            {
+                ErrMsg.Text = "Please scan an ID.";
+                ErrMsg.ForeColor = Color.Red;
+                ErrMsg.Font = new Font("Times New Roman", 20);
+            }
+        }
+
+        private void printPreviewDialog1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        public void Printing()
+        {
+            try
+            {
+                PrintDocument pd = new PrintDocument();
+                pd.PrintPage += new PrintPageEventHandler(printDocument1_PrintPage);
+                pd.PrinterSettings.PrinterName = "ZDesigner S4M-203dpi ZPL";
+                // Specify the printer to use.
+
+                if (pd.PrinterSettings.IsValid)
+                {
+                    pd.Print();
+                }
+                else
+                {
+                    MessageBox.Show("Printer is invalid.");
+                }
+            }   
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
         }
     }
 }
